42ffdc381d59ed336d0a48bb4406ff8c1d73ef43
KeyForce <july.master@outlook.com>
2019-09-05 16:27:52 +0800

    New

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from models.vgg_test import *
 from models import *
 from utils import progress_bar
 from visualzation.confusion_matrix import plot_confusion_matrix
+import torchvision
+import matplotlib.pyplot as plt
+from torch.utils.data.dataset import ConcatDataset
 
 # 参数解析
 parser = argparse.ArgumentParser(description='PyTorch Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint', default=False)
+parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint', default=True)
 args = parser.parse_args()
 
 # 设置GPU
@@ -25,6 +28,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 classes = ('0', '1')  # 必须是数值型
+classes_str = ['benign', 'suspicious']
 
 # 定义自己的数据集 需要设计自己的解析代码
 class MyDataset(Dataset):
@@ -58,25 +62,70 @@ class MyDataset(Dataset):
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.CenterCrop(96),
+    transforms.CenterCrop(224),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
     transforms.ToTensor(),
+    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
 
 transform_test = transforms.Compose([
-    transforms.CenterCrop(96),
+    transforms.CenterCrop(224),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    transforms.ToTensor(),
+    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+transform_benign = transforms.Compose([
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(1),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
     transforms.ToTensor(),
 ])
 
-root = '/home/hanwei-1/data/usg/cvted1-3/ROI'
+transform_benign_two = transforms.Compose([
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(5),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    transforms.ToTensor(),
+])
+
+transform_benign_three = transforms.Compose([
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(5),
+    transforms.ColorJitter(brightness=0.5, contrast=0.6, saturation=0.2, hue=0.1),
+    transforms.ToTensor(),
+])
+
+root = '/home/hanwei-1/data/usg/ROI'
 
 # 根据自己定义的那个勒MyDataset来创建数据集！注意是数据集！而不是loader迭代器
-train_data = MyDataset(root, '/train_aug.txt', transform=transform_train)
-test_data = MyDataset(root, '/test_aug.txt', transform=transform_test)
+train_data = MyDataset(root, '/train.txt', transform=transform_train)
+test_data = MyDataset(root, '/test.txt', transform=transform_test)
+aug_benign = torchvision.datasets.ImageFolder(root='/home/hanwei-1/data/usg/ROI/Aug/',
+                                              transform=transform_benign
+                                              )
+aug_benign_two = torchvision.datasets.ImageFolder(root='/home/hanwei-1/data/usg/ROI/Aug/',
+                                              transform=transform_benign_two
+                                              )
+aug_benign_three = torchvision.datasets.ImageFolder(root='/home/hanwei-1/data/usg/ROI/Aug/',
+                                              transform=transform_benign_three
+                                              )
+
+train_data = ConcatDataset([train_data, aug_benign, aug_benign_two, aug_benign_three])
+train_test = ConcatDataset([train_data, test_data])
 
 # 然后就是调用DataLoader和刚刚创建的数据集，来创建dataloader，loader的长度是有多少个batch，所以和batch_size有关
-trainloader = DataLoader(dataset=train_data, batch_size=2, shuffle=True)
-testloader = DataLoader(dataset=test_data, batch_size=2)
+trainloader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
+testloader = DataLoader(dataset=test_data, batch_size=64)
+train_test_loader = DataLoader(dataset=train_test, batch_size=64, shuffle=True)
+image = iter(aug_benign)
 
+image, labels = next(image)
+img = torchvision.utils.make_grid(image)
+plt.imshow(img.numpy().transpose(1, 2, 0))
+plt.show()
 
 
 # 网络
@@ -100,21 +149,21 @@ net = vgg19_bn(num_classes=2)
 # net = vgg19_bn(num_classes=2, init_weights=True, pretrained=False)
 net = net.to(device)
 if device == 'cuda':
-    net = torch.nn.DataParallel(net)
+    net = torch.nn.DataParallel(net, device_ids=[0, 1])
     cudnn.benchmark = True
 
 if args.resume:
     # 加载 checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    checkpoint = torch.load('./checkpoint/vggckpt.pth')
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
-criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-optimizer = optim.Adam(net.parameters(), lr=args.lr)
+criterion = nn.CrossEntropyLoss().cuda(device)
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+# optimizer = optim.Adam(net.parameters(), lr=args.lr)
 
 # Training
 def train(epoch):
@@ -149,7 +198,7 @@ def Test(epoch):
     cm_targets = []
     cm_predicted = []
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
+        for batch_idx, (inputs, targets) in enumerate(train_test_loader):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
             loss = criterion(outputs, targets)
@@ -167,7 +216,7 @@ def Test(epoch):
                          % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
 
     matrix = confusion_matrix(cm_targets, cm_predicted)
-    plot_confusion_matrix(matrix, classes)
+    plot_confusion_matrix(matrix, classes_str)
     # Save checkpoint.
     acc = 100. * correct / total
     if acc > best_acc:
